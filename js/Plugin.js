44395daf1a512ea6f81eee3a19e6c239c77644ca
eMaj-Fr <renaud.tertrais@gmail.com>
2013-09-06 19:13:49 +0200

    add demo

--- js/Plugin.js
+++ js/Plugin.js
@@ -1,13 +1,12 @@
 function Plugin( name, methods, defaults){
-    // just create some reference to access to methods and default properties outside of this function (OOP)
-    this.methods = $.extend(methods,{});
-    this.defaults = $.extend(defaults,{});
+    // just create some references to access to methods and default properties outside of this function (OOP)
+    this.methods = $.extend({},methods);
+    this.defaults = $.extend({},defaults);
     methods = this.methods;
     defaults = this.defaults;
-  
     $.fn[name] = function(first , second){
         return this.each(function(){
-            var $this = $(this);
+            var $this = $(this);           
             // first is a string so you want to call a method, but check if this[name] exist => init the plugin
             if(this[name] && typeof(first)=="string"){
                 if($.fn[name].methods.hasOwnProperty(first)){
@@ -21,7 +20,7 @@ function Plugin( name, methods, defaults){
                     options = $.extend(this[name].options,options);
                 // this[name] doesn't exist so this is first time : init methods and merge defaults aud custum properties
                 }else{
-                    this[name] = {};
+                    this[name] = {};                   
                     options = $.extend(defaults,options);
                     this[name].options = options;
                     $.fn[name].methods = methods;
